bc4ca5eaab90f38e9f99c217b03d269642ce8c95
Muhammad Dahnil <dahnil@realta.net>
2024-11-14 11:50:23 +0700

    first commit

---
+++ assets/js/pages/forgot-password.js
@@ -0,0 +1,6 @@
+// $(function () {
+
+// 	  $('#forgotPassword').load('pages/forgotPassword.html',function(){
+//     	$.getScript('assets/js/pages/home.js');
+//     });
+// });
---
+++ assets/js/pages/sign-in.js
@@ -0,0 +1,21 @@
+// function changePage()
+// {
+// 	 window.location.href = "http://localhost/EssTen10/pages/forgot-password.html";
+// };
+
+ 
+// $(function () {
+
+// 	  $('#forgotPassword').load('pages/forgot-password.html',function(){
+//     	$.getScript('assets/js/pages/forgot-password.js');
+//     });
+
+//     // $("#profile").click(function(){
+//     //   $('#contentPages').load('pages/profile.html',function(){
+//     //         $.getScript('assets/js/pages/profile.js');
+//     //      });
+//     // });
+
+   
+
+// });
